f0ddb3c01340cb168ed77774dc8f366ed5f2df23
Jarrad Miller <jm020801@gmail.com>
2019-01-25 15:46:01 -0700

    updated sprint

--- README.md
+++ README.md
@@ -29,9 +29,21 @@ Implement an User Authentication System in order to access the jokes from the Jo
 Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 1. What is the purpose of using _sessions_?
+
+<!-- The purpose is so that sessions can store data with a unique ID. You won't have to login everytime you click on a new page within that website -->
+
 1. What does bcrypt do to help us store passwords in a secure manner.
+
+<!--  It encrypts or hashes the password making it alot harder to hack. -->
+
 1. What does bcrypt do to slow down attackers?
+
+<!-- It can make a weak password longer and more secure using Key Stretching. -->
+
 1. What are the three parts of the JSON Web Token?
+
+<!-- Header Payload Signature -->
+
 
 ## Project Set Up
 
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../database/dbConfig");
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,14 +11,72 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(payload) {
+  return jwt.sign(
+    payload, process.env.SECRET || "randomString", 
+    {
+      expiresIn: "1hr"
+    }
+  )
+}
+
+
 function register(req, res) {
   // implement user registration
+  let { username, password } = req.body;
+
+  if (!username || !password)
+    return res.json({
+      error: true,
+      message: "You need BOTH a username and password."
+    });
+
+  password = bcrypt.hashSync(password, 3);
+
+  db("users")
+    .insert({ username, password })
+    .then(([id]) => {
+      let token = generateToken({ id });
+      res.json({
+        error: false,
+        message: "User Created Successfully",
+        token
+      });
+    })
+    .catch(err => res.json(err));
 }
+
 
 function login(req, res) {
   // implement user login
-}
+  let { username, password } = req.body;
 
+  if (!username || !password)
+    return res.json({
+      error: true,
+      message: "You need BOTH a username and a password."
+    });
+
+  db("users")
+    .where({ username: username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        let token = generateToken(user);
+        res.json({
+          error: false,
+          message: `Welcome ${username}`,
+          token
+        });
+      } else {
+        return register.json({
+          error: true,
+          message: "Login credentials not working."
+        });
+      }
+    })
+    .catch(err => res.json(err));
+}
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: 'application/json' },
